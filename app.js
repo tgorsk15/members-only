524b1de7c2ac5516245498371392656edc88b081
tgorsk15 <tgbusiness463@gmail.com>
2024-08-20 17:36:25 -0500

    Install dependencies and create file structure

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }))
 
+app.use(express.static(assetsPath))
+
 // use routers here
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`always watchin you on ${PORT}`))
