7f01c2ac12f9d3f6d54c8c3c36abbb5511605693
tgorsk15 <tgbusiness463@gmail.com>
2024-08-20 17:49:38 -0500

    Develop app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
-// import routers here
+// import routers
+const indexRouter = require('./routes/index')
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }))
 
+const assetsPath = path.join(__dirname, "public")
 app.use(express.static(assetsPath))
 
 // use routers here
-
+app.use("/", indexRouter)
 
 
 const PORT = process.env.PORT || 5000
