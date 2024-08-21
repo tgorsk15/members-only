672980bd942ca4854bf68d6e2ec86feae4f21e76
tgorsk15 <tgbusiness463@gmail.com>
2024-08-21 08:48:23 -0500

    Create pool and connect to Railway DB

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path')
 const app = express();
+require('dotenv').config();
 // import routers
 const indexRouter = require('./routes/index')
 
@@ -14,5 +15,6 @@ app.use(express.static(assetsPath))
 app.use("/", indexRouter)
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 4000
+console.log(PORT)
 app.listen(PORT, () => console.log(`always watchin you on ${PORT}`))
