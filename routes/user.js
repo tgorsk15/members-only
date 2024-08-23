8caa86a3ebfb5064e389b3eeffc71cd3fb957e24
tgorsk15 <tgbusiness463@gmail.com>
2024-08-23 15:06:54 -0500

    Restructure homePage so that login is a separate view

--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,8 @@ const usersController = require('../controllers/usersController');
 userRouter.get("/new", usersController.signupFormGet)
 userRouter.post("/new", usersController.signupPost)
 
+// for login
+userRouter.get("/login", usersController.loginFormGet)
+
 
 module.exports = userRouter
