6b01d0899335a1d77ccb665b511d158a5d757a2d
tgorsk15 <tgbusiness463@gmail.com>
2024-08-23 12:35:02 -0500

    Develop passport.js authentication logic

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const path = require('path')
 const app = express();
+const session = require('express-session')
+const pool = require('./db/pool')
+const pgSession = require('connect-pg-simple')(session);
+
 require('dotenv').config();
+require('./auth/passport')
+
+// set up store
+const sessionStore = new pgSession({
+    pool: pool,
+})
+console.log(sessionStore)
+
 // import routers
 const indexRouter = require('./routes/index')
 const userRouter = require('./routes/user')
 const postRouter = require('./routes/post')
 
+
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }))
+
+// set up session:
+app.use(session({
+    store: sessionStore,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}))
+
 
 const assetsPath = path.join(__dirname, "public")
 app.use(express.static(assetsPath))
@@ -17,7 +42,6 @@ app.use(express.static(assetsPath))
 app.use("/user", userRouter);
 app.use("/post", postRouter);
 app.use("/", indexRouter);
-
 
 
 const PORT = process.env.PORT || 4000
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,5 @@
 const db = require("../db/queries")
+const bcrypt = require('bcryptjs')
 
 exports.homePageGet = async (req,res) => {
     console.log(req.body)
@@ -12,4 +13,24 @@ exports.signupFormGet = async (req, res) => {
     res.render('signup', {
         title: 'Sign Up'
     })
+}
+
+exports.signupPost = async (req, res, next) => {
+    // use passport to store user in session
+    try {
+        console.log(req.body)
+        const userInfo = req.body
+        userInfo.isMember = false
+        userInfo.isAdmin = false
+        console.log(userInfo)
+        
+        bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+            console.log(hashedPassword)
+            await db.insertNewUser(userInfo)
+        })
+    } catch(err) {
+
+    }
+
+    console.log('profile created')
 }
