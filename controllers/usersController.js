5c7bfcfee4dcb7b6cd9d1420ba5e2688c45684f0
tgorsk15 <tgbusiness463@gmail.com>
2024-08-25 20:13:11 -0500

    Add form validation to sign up form

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,32 @@
 const db = require("../db/queries")
 const passport = require("passport")
 const bcrypt = require('bcryptjs')
+const { body, validationResult } = require("express-validator")
+
+const nameErr = "First and Last name both need to be between 2 and 20 characters"
+const usernameErr = "Username must be between 5 and 25 characters"
+const passwordErr = "Password must be at least 7 character long"
+
+const validateUser = [
+    body("firstName").trim()
+        .isLength({ min: 2, max: 20 }).withMessage(nameErr),
+    body("lastName").trim()
+        .isLength({ min: 2, max: 20 }).withMessage(nameErr),
+    body("username").trim()
+        .isLength({ min: 5, max: 25}).withMessage(usernameErr),
+    body("password").trim()
+    .isLength({ min: 7 }).withMessage(passwordErr)
+        .custom((value) => {
+            const specialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+            if (!specialChar.test(value)){
+                throw new Error("Password must include at least one special character")
+            }
+            return true
+        })
+
+    
+]
+
 
 exports.homePageGet = async (req,res) => {
     console.log(req.body)
@@ -62,27 +88,39 @@ exports.signupFormGet = async (req, res) => {
     })
 }
 
-exports.signupPost = async (req, res, next) => {
-    // use passport to store user in session
-    try {
-        const userInfo = req.body
-        userInfo.isMember = false
-        userInfo.isAdmin = false
-        console.log(userInfo)
-        
-        bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
-            console.log(hashedPassword)
-            await db.insertNewUser(userInfo, hashedPassword)
-        })
+exports.signupPost = [
+    validateUser,
+    async (req, res, next) => {
+        try {
+            const errors = validationResult(req);
 
-        // if it works, let user know it worked
-        res.render("signup", {
-            title: 'Sign Up',
-            joined: true
-        })
-    } catch(err) {
-        return next(err)
+            if (!errors.isEmpty()) {
+                return res.status(400).render("signup", {
+                    title: "Sign Up",
+                    errors: errors.array()
+                })
+            };
+
+            const userInfo = req.body
+            userInfo.isMember = false
+            userInfo.isAdmin = false
+            console.log(userInfo)
+            
+            bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+                console.log(hashedPassword)
+                await db.insertNewUser(userInfo, hashedPassword)
+            })
+
+            // if it works, let user know it worked
+            res.render("signup", {
+                title: 'Sign Up',
+                joined: true
+            })
+        } catch(err) {
+            return next(err)
+        }
     }
+]
 
-    console.log('profile created')
-}
+    
+
